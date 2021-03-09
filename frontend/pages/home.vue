0e0f7f039acd0637bd4c259f448c82eef807120c
Katy <peerasorn14@gmail.com>
2021-03-09 22:08:22 +0700

    Example pages

---
+++ frontend/pages/home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-6xl text-red-400 bg-green-200">This is a home</h1>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1 class="text-6xl text-red-400 bg-green-200">Ahoy! Nuxt.js</h1>
+  <div>
+    <span class="text-4xl">{{ count }}</span>
+    <button class="border bg-red-200 p-4" @click="decreaseCount()">-</button>
+    <button class="border bg-red-200 p-4" @click="increaseCount()">+</button>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    increaseCount() {
+      this.count = this.count + 1;
+    },
+
+    decreaseCount() {
+      this.count = this.count - 1;
+    },
+  },
+};
+</script>
